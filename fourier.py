a7e9c57162a40a3273cedd3ab5ded29dcfbcde14
l.fricke <l.fricke@frox-it.de>
2019-09-18 18:21:53 +0200

    first stuff drwaing

--- fourier.py
+++ fourier.py
@@ -1,6 +1,8 @@
 import pygame
 import math
 
+PI = 3.141592653589793
+
 def draw_arrow(screen, start, end, color, width=1, head=10):
   pygame.draw.line(screen,color,start,end,width)
   rotation = math.degrees(math.atan2(start[1]-end[1], end[0]-start[0]))+90
@@ -8,7 +10,7 @@ def draw_arrow(screen, start, end, color, width=1, head=10):
 
 
 def drawComplex(screen, base, num, color):
-  draw_arrow(screen, (base.real, base.imag), (base.real+num.real, base.real+num.imag), color, 2, 8)
+  draw_arrow(screen, (base.real, base.imag), (base.real+num.real, base.imag+num.imag), color, 2, 4)
 
 def fromPolar(r, phi):
   return r * (math.cos(phi) + math.sin(phi)*1j)
@@ -17,22 +19,20 @@ def fromPolar(r, phi):
 successes, failures = pygame.init()
 print("{0} successes and {1} failures".format(successes, failures))
 
-
-screen = pygame.display.set_mode((480, 480))
+WINDOW_SIZE = (720, 720)
+screen = pygame.display.set_mode(WINDOW_SIZE)
 clock = pygame.time.Clock()
 FPS = 60  # Frames per second.
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-PI = 3.141592653589793
+root = complex(WINDOW_SIZE[0]/2, WINDOW_SIZE[1]/2)
 
 t = 0.0
 while True:
     dt = clock.tick(FPS) / 1000
-    t += dt
-    #while t >= 1.0:
-    #  t -= 1.0
+    t += dt*0.1
 
     screen.fill(BLACK)
 
@@ -40,7 +40,13 @@ while True:
         if event.type == pygame.QUIT:
           quit()
 
-    c = fromPolar(50, 1 * 2 * PI * t)
+    p = root
+    for i in range(-3, 3):
+      c = fromPolar(25.0, i * 2 * PI * t)
+      #print(c)
+      drawComplex(screen, p, c, WHITE)
+      p += c
     
-    drawComplex(screen, complex(100, 100), c, WHITE)
     pygame.display.update()
+
+print("Exited!")
