b8c079d766b378c4759e761f496d3128dd01c9e2
l.fricke <l.fricke@frox-it.de>
2019-09-18 19:23:56 +0200

    intergration calculus is cool

--- fourier.py
+++ fourier.py
@@ -13,30 +13,43 @@ def draw_arrow(screen, start, end, color, width=1, head=10):
 def drawComplex(screen, base, num, color):
   draw_arrow(screen, (base.real, base.imag), (base.real+num.real, base.imag+num.imag), color, 2, 4)
 
-def fromPolar(r, phi):
+def polar(r, phi):
   return r * (math.cos(phi) + math.sin(phi)*1j)
   
+def approximateCs(count, func, integration_steps=100):
+  cs = []
+  halfcount = int(count/2)
 
-successes, failures = pygame.init()
-print("{0} successes and {1} failures".format(successes, failures))
+  for i in range(-halfcount, halfcount+1):
+    integ = complex(0.0, 0.0)
+    deltaT = 1.0 / integration_steps
+    for j in range(integration_steps):
+      integ += deltaT * func(j * deltaT) * polar(1.0, -i * 2 * PI * (j * deltaT))
+    cs.append((i, integ))
+  return cs
 
-WINDOW_SIZE = (720, 720)
-screen = pygame.display.set_mode(WINDOW_SIZE)
-clock = pygame.time.Clock()
-FPS = 60  # Frames per second.
+def example1(t):
+  return polar(40*math.sin(2*PI*t)+50, 2 * PI * t)
 
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
+def main():
+  successes, failures = pygame.init()
+  print("{0} successes and {1} failures".format(successes, failures))
 
-root = complex(WINDOW_SIZE[0]/2, WINDOW_SIZE[1]/2)
-series_count=25
-halfcount = int(series_count/2)
+  WINDOW_SIZE = (720, 720)
+  screen = pygame.display.set_mode(WINDOW_SIZE)
+  clock = pygame.time.Clock()
+  FPS = 60  # Frames per second.
 
-cs = [(i, complex(25*random.uniform(-1,1), random.uniform(-1,1))) for i in range(-halfcount, halfcount+1)]
+  BLACK = (0, 0, 0)
+  WHITE = (255, 255, 255)
 
-timescale=0.1
-t = 0.0
-while True:
+  root = complex(WINDOW_SIZE[0]/2, WINDOW_SIZE[1]/2)
+
+  cs = approximateCs(30, example1)
+
+  timescale=0.1
+  t = 0.0
+  while True:
     dt = clock.tick(FPS) / 1000
     if t < 1.0:
       t += dt*timescale
@@ -49,13 +62,18 @@ while True:
         elif event.type == pygame.KEYDOWN:
           if event.key == pygame.K_SPACE:
             t = 0
+          elif event.key == pygame.K_ESCAPE:
+            quit()
 
     p = root
     for (i, c) in cs:
-      n = c * fromPolar(1.0, i * 2 * PI * t)
+      n = c * polar(1.0, i * 2 * PI * t)
       drawComplex(screen, p, n, WHITE)
       p += n
     
     pygame.display.update()
 
-print("Exited!")
+  print("Exited!")
+
+if __name__ == '__main__':
+  main()
